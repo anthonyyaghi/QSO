02cbdd07aaac608256b30eb342f319d7fdf07459
anthonyyaghi <anthonyyaghi@outlook.com>
2020-11-14 13:15:49 +0200

    fix logging interval

--- qso_CIFAR10.py
+++ qso_CIFAR10.py
@@ -14,6 +14,7 @@ from torch.optim.lr_scheduler import StepLR
 import matplotlib.pyplot as plt
 from scipy.stats import norm
 import time
+import math
 from torch.optim.optimizer import Optimizer, required
 
 
@@ -102,18 +103,19 @@ class QSOA(Optimizer):
         return loss
 
 
+batch_size = 32
 transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 train_set = torchvision.datasets.CIFAR10(root='./data', train=True,
                                          download=True, transform=transform)
-train_loader = torch.utils.data.DataLoader(train_set, batch_size=32,
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size,
                                            shuffle=True, num_workers=2)
 
 test_set = torchvision.datasets.CIFAR10(root='./data', train=False,
                                         download=True, transform=transform)
-test_loader = torch.utils.data.DataLoader(test_set, batch_size=32,
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size,
                                           shuffle=False, num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat',
@@ -159,7 +161,7 @@ def test():
 n_epochs = 10
 save_epochs = 5
 
-log_interval = 2500
+log_interval = math.ceil(50000/batch_size/35)
 # random_seed = torch.randint(0, 100, (1, 1)).item()
 random_seed = 16
 torch.backends.cudnn.enabled = True
